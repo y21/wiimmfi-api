98157506f29cc419c462426f99a416e317891b22
Timo <30553356+y21@users.noreply.github.com>
2018-04-29 01:24:41 +0200

    Home redirects to amount endpoint yet

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const { readdirSync } = require('fs');
 
 for(let d of readdirSync('./routes/')){
-  app.get('/' + d.split(".")[0], require(`./routes/${d}`));
+  if(d !== 'routes') app.get('/' + d.split(".")[0], require(`./routes/${d}`));
 }
+app.get('/', (req,res)=>res.send('<script>document.location.href="amount/";</script>')); // redirect to /amount/ yet
+
 
 app.listen(3000, ()=>console.log('Application started!'));
