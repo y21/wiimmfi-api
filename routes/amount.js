c28d3495d351e4718c83672154a2a55089143a33
Timo <30553356+y21@users.noreply.github.com>
2018-04-29 18:48:50 +0200

    More information + amount of ALL rooms

--- routes/amount.js
+++ routes/amount.js
@@ -7,7 +7,16 @@ module.exports = (req, res) => {
       str += d;
     });
     re.on('end', () => {
-      res.json({ status: 200, rooms: str.split(/<img src="\/images\/watch-room-24x16\.png" *class="text-img"><\/a>/).length });
+        let result = {
+            status: 200,
+            totalAvailable: {
+                amount: str.split(/<img src="\/images\/watch-room-24x16\.png" *class="text-img"><\/a>/).length-1,
+            },
+            total: {
+                amount: str.split(/friend code/).length-1
+            }
+        };
+      res.json(result);
     });
   });
 }
