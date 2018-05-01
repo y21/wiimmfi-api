8d31f1900e11b30edbadb1e8f2d0a0f32043128c
Timo <30553356+y21@users.noreply.github.com>
2018-05-01 21:02:17 +0200

    Comments, crash fix, use db.all and more

--- utils/updateData.js
+++ utils/updateData.js
@@ -15,6 +15,7 @@ module.exports = (db) => {
         re.on('end', () => {
             const availableRoomsSource = str.substr(0, str.search(/List all MKW rooms\.<\/a><br\/>/));
             result = {
+                // amount endpoint
                 status: 200,
                 totalAvailable: {
                     worldwides: availableRoomsSource.split(/Worldwide *room/i).length-1,
@@ -28,11 +29,33 @@ module.exports = (db) => {
                     privates: str.split(/private room/i).length-1,
                     players: str.split(/<td>.{1,28}<\/td>/).length-1
                 },
+
+                // regions endpoint (for available rooms)
+                ctgp: availableRoomsSource.split(/<td align *= *"center" *> *CTGP *<\/td *>/).length-1,
+                eur: availableRoomsSource.split(/<td align *= *"center" *> *Eur\/2 *<\/td *>/).length-1,
+                jap: availableRoomsSource.split(/<td align *= *"center" *> *Jap\/0 *<\/td *>/).length-1,
+                ame: availableRoomsSource.split(/<td align *= *"center" *> *Ame\/1 *<\/td *>/).length-1
             };
-            db.get('select * from totalAmount').then(r => {
-                if(!r) return db.run(`insert into totalAmount values ('${result.total.worldwides}', '${result.total.continentals}', '${result.total.privates}', '${result.totalAvailable.worldwides}', '${result.totalAvailable.continentals}', '${result.totalAvailable.privates}', '${Date.now()}', '${result.totalAvailable.players}', '${result.total.players}')`).catch(console.log);
+
+
+            // ---------------------
+            // Queries
+            // ---------------------
+
+
+            //  TotalAmount Queries
+            db.all('select * from totalAmount').then(r => {
+                if(typeof r === 'undefined') return db.run('CREATE TABLE "totalAmount" ( `totalWWs` TEXT, `totalContinentals` TEXT, `totalPrivates` TEXT, `WWs` TEXT, `continentals` TEXT, `privates` TEXT, `lastEdit` TEXT, `players` TEXT, `totalPlayers` TEXT )');
+                else if(r.length == 0) return db.run(`insert into totalAmount values ('${result.total.worldwides}', '${result.total.continentals}', '${result.total.privates}', '${result.totalAvailable.worldwides}', '${result.totalAvailable.continentals}', '${result.totalAvailable.privates}', '${Date.now()}', '${result.totalAvailable.players}', '${result.total.players}')`).catch(console.log);
                 else db.run(`update totalAmount set totalWWs='${result.total.worldwides || 0}', totalContinentals='${result.total.continentals || 0}', totalPrivates='${result.total.privates || 0}', WWs='${result.totalAvailable.worldwides || 0}', continentals='${result.totalAvailable.continentals || 0}', privates='${result.totalAvailable.privates || 0}', lastEdit='${Date.now()}', players='${result.totalAvailable.players}', totalPlayers='${result.total.players}'`).catch(console.log);
-            });
+            }).catch(console.log);
+
+            // Regions Queries
+            db.all('select * from region_amount').then(r => {
+                if(typeof r === 'undefined') return db.run('CREATE TABLE `region_amount` ( `eur` INTEGER, `jap` INTEGER, `ctgp` INTEGER, `ame` INTEGER )');
+                else if(r.length == 0) return db.run(`insert into region_amount values('${result.eur}', '${result.jap}', '${result.ctgp}', '${result.ame}')`);
+                else db.run(`update region_amount set eur='${result.eur}', jap='${result.jap}', ctgp='${result.ctgp}', ame='${result.ame}'`);
+            }).catch(console.log);
         });
     });
 };
