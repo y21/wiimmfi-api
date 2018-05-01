100a121b2e5f9aa040d1621bb01bfabea85bd632
Timo <30553356+y21@users.noreply.github.com>
2018-05-01 19:28:36 +0200

    Update every 30sec PLS!

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express'), app = express(), { readdirSync } = require('fs'), { get } = require('https');
-let routes = readdirSync('./routes/');
+const express = require('express'), app = express(), { readdirSync } = require('fs'), { get } = require('https'), fs = require('fs');
 const Base = require('./Base');
 Base.initUtils().then(u => console.log(`Successfully loaded ${Object.keys(u).length} utilities.`)).catch(console.log);
 
-for(let d of routes){
-  if(d === 'routes') return;
-  app.get('/' + d.split(".")[0], require(`./routes/${d}`));
+for(let d of fs.readdirSync('./routes/')){
+  if(d !== 'routes') {
+      app.get('/' + d.split(".")[0], require(`./routes/${d}`));
+  }
 }
 
 app.get('/', (req,res)=>res.send('<script>document.location.href="amount/";</script>')); // redirect to /amount/ yet
@@ -13,7 +13,7 @@ app.get('/', (req,res)=>res.send('<script>document.location.href="amount/";</scr
 
 
 setTimeout(()=>Base.utils.updateData(Base.db), 5000); // wait 5 seconds after startup
-setInterval(()=>Base.utils.updateData(Base.db), 120000); // update data every 2 minutes
+setInterval(()=>Base.utils.updateData(Base.db), 30000); // update data every 30 seconds
 
 
 
