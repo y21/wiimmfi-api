1910697c6d6d36bd3fbcd5a50872bd3708bd9724
Timo <30553356+y21@users.noreply.github.com>
2018-05-02 19:54:08 +0200

    Wiimmfi frontend has changed

--- utils/updateData.js
+++ utils/updateData.js
@@ -13,28 +13,21 @@ module.exports = (db) => {
             str += d;
         });
         re.on('end', () => {
-            const availableRoomsSource = str.substr(0, str.search(/List all MKW rooms\.<\/a><br\/>/));
             result = {
                 // amount endpoint
                 status: 200,
                 totalAvailable: {
-                    worldwides: availableRoomsSource.split(/Worldwide *room/i).length-1,
-                    continentals: availableRoomsSource.split(/Continental *room/i).length-1,
-                    privates: availableRoomsSource.split(/Private *room/).length-1,
-                    players: availableRoomsSource.split(/<td>.{1,28}<\/td>/).length-1
-                },
-                total: {
-                    worldwides: str.split(/worldwide room/i).length-1,
-                    continentals: str.split(/continental room/i).length-1,
-                    privates: str.split(/private room/i).length-1,
+                    worldwides: str.split(/Worldwide *room/i).length-1,
+                    continentals: str.split(/Continental *room/i).length-1,
+                    privates: str.split(/Private *room/).length-1,
                     players: str.split(/<td>.{1,28}<\/td>/).length-1
                 },
 
                 // regions endpoint (for available rooms)
-                ctgp: availableRoomsSource.split(/<td align *= *"center" *> *CTGP *<\/td *>/).length-1,
-                eur: availableRoomsSource.split(/<td align *= *"center" *> *Eur\/2 *<\/td *>/).length-1,
-                jap: availableRoomsSource.split(/<td align *= *"center" *> *Jap\/0 *<\/td *>/).length-1,
-                ame: availableRoomsSource.split(/<td align *= *"center" *> *Ame\/1 *<\/td *>/).length-1
+                ctgp: str.split(/<td align *= *"center" *> *CTGP *<\/td *>/).length-1,
+                eur: str.split(/<td align *= *"center" *> *Eur\/2 *<\/td *>/).length-1,
+                jap: str.split(/<td align *= *"center" *> *Jap\/0 *<\/td *>/).length-1,
+                ame: str.split(/<td align *= *"center" *> *Ame\/1 *<\/td *>/).length-1
             };
 
 
@@ -45,12 +38,12 @@ module.exports = (db) => {
 
             //  TotalAmount Queries
             db.all('select * from totalAmount').then(r => {
-                if(typeof r === 'undefined') return db.run('CREATE TABLE "totalAmount" ( `totalWWs` TEXT, `totalContinentals` TEXT, `totalPrivates` TEXT, `WWs` TEXT, `continentals` TEXT, `privates` TEXT, `lastEdit` TEXT, `players` TEXT, `totalPlayers` TEXT )');
-                else if(r.length == 0) return db.run(`insert into totalAmount values ('${result.total.worldwides}', '${result.total.continentals}', '${result.total.privates}', '${result.totalAvailable.worldwides}', '${result.totalAvailable.continentals}', '${result.totalAvailable.privates}', '${Date.now()}', '${result.totalAvailable.players}', '${result.total.players}')`).catch(console.log);
-                else db.run(`update totalAmount set totalWWs='${result.total.worldwides || 0}', totalContinentals='${result.total.continentals || 0}', totalPrivates='${result.total.privates || 0}', WWs='${result.totalAvailable.worldwides || 0}', continentals='${result.totalAvailable.continentals || 0}', privates='${result.totalAvailable.privates || 0}', lastEdit='${Date.now()}', players='${result.totalAvailable.players}', totalPlayers='${result.total.players}'`).catch(console.log);
+                if(typeof r === 'undefined') return db.run('CREATE TABLE "totalAmount" ( `WWs` TEXT, `continentals` TEXT, `privates` TEXT, `lastEdit` TEXT, `players` TEXT )');
+                else if(r.length == 0) return db.run(`insert into totalAmount values ('${result.totalAvailable.worldwides}', '${result.totalAvailable.continentals}', '${result.totalAvailable.privates}', '${Date.now()}', '${result.totalAvailable.players}')`).catch(console.log);
+                else db.run(`update totalAmount set WWs='${result.totalAvailable.worldwides || 0}', continentals='${result.totalAvailable.continentals || 0}', privates='${result.totalAvailable.privates || 0}', lastEdit='${Date.now()}', players='${result.totalAvailable.players}'`).catch(console.log);
             }).catch(error => {
                 if(error.toString().includes('no such table: totalAmount')){
-                    return db.run('CREATE TABLE "totalAmount" ( `totalWWs` TEXT, `totalContinentals` TEXT, `totalPrivates` TEXT, `WWs` TEXT, `continentals` TEXT, `privates` TEXT, `lastEdit` TEXT, `players` TEXT, `totalPlayers` TEXT )');
+                    return db.run('CREATE TABLE "totalAmount" ( `WWs` TEXT, `continentals` TEXT, `privates` TEXT, `lastEdit` TEXT, `players` TEXT )');
                 }
                 else console.log(error.toString());
             });
