7da472a2f59e8ac245bc3bf162a075ce292f3037
Timo <30553356+y21@users.noreply.github.com>
2018-05-07 20:56:39 +0200

    Add room property

--- routes/findUser.js
+++ routes/findUser.js
@@ -12,7 +12,7 @@ module.exports = (req, res) => {
         let str = '';
         result.on('data', d => str += d);
         result.on('end', () => {
-            str = str.substr(str.indexOf('<th>friend code</th>')); // /*.replace(/\*/g, '\*').replace(/\+/g, '\+')
+            str = str.substr(str.indexOf('<th>friend code</th>'));
             const userMatch = (str.match(new RegExp('<td align="center">(CTGP|Eur|Jap|Ame)\\/\\d?<\\/td>\\s*<td align="center">.{1,16}<\\/td>\\s*<td align="center">.{1,16}<\\/td>\\s*<td align="center".{1,128}<\\/td>\\s*<td align="center">.{1,16}<\\/td>\\s*<td align="center">.{1,16}<\\/td>\\s*<td>' + req.query.name + '<\\/td>', 'g')) || []).map(e=>{
                 return e.split(/(<td align="center">|<\/td>|<td align="center">|<td align="center" title="[\w\s]+">|<td>)/)
             });
@@ -24,7 +24,8 @@ module.exports = (req, res) => {
                     BR: ((userMatch[0] || []).filter(e=>/\d{4}/g.test(e)) || []).sort((a,b)=>str.indexOf(a)-str.indexOf(b))[0],
                     loginRegion: (userMatch[0] || [])[2],
                     gameType: (userMatch[0] || []).find(e=>e.includes('vs')) ? "versus" : "battle",
-                    connectionFail: (userMatch[0] || []).find(e=>/\d\.\d{2}$/.test(e)) ? userMatch[0].find(e=>/\d\.\d{2}$/.test(e)).substr(userMatch[0].find(e=>/\d\.\d{2}$/.test(e)).search(/\d\.\d{2}/)) : false
+                    connectionFail: (userMatch[0] || []).find(e=>/\d\.\d{2}$/.test(e)) ? userMatch[0].find(e=>/\d\.\d{2}$/.test(e)).substr(userMatch[0].find(e=>/\d\.\d{2}$/.test(e)).search(/\d\.\d{2}/)) : false,
+                    room: str.substr(str.substring(0, str.indexOf('<td>' + req.query.name + '</td>')).lastIndexOf('</a> (created ')-4, 4) || '-'
                 }
             });
         });
