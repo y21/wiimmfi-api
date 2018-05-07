8259075b7fa97fef2d5dc9b7fb5340e97c7b5c67
Timo <30553356+y21@users.noreply.github.com>
2018-05-07 19:57:22 +0200

    Fix swapped vr/br

--- routes/findUser.js
+++ routes/findUser.js
@@ -16,13 +16,12 @@ module.exports = (req, res) => {
             const userMatch = (str.match(new RegExp('<td align="center">(CTGP|Eur|Jap|Ame)\\/\\d?<\\/td>\\s*<td align="center">.{1,16}<\\/td>\\s*<td align="center">.{1,16}<\\/td>\\s*<td align="center".{1,128}<\\/td>\\s*<td align="center">.{1,16}<\\/td>\\s*<td align="center">.{1,16}<\\/td>\\s*<td>' + req.query.name + '<\\/td>', 'g')) || []).map(e=>{
                 return e.split(/(<td align="center">|<\/td>|<td align="center">|<td align="center" title="[\w\s]+">|<td>)/)
             });
-            str = str.substr(str.indexOf('<th>friend code</th>'));
             if(!userMatch[0]) return res.json({ status: 400, message: "user not found" });
             res.json({
                 status: 200,
                 data: {
-                    VR: ((userMatch[0] || []).filter(e=>/\d{4}/g.test(e)) || []).sort((a,b)=>str.indexOf(a)-str.indexOf(b))[0],
-                    BR: ((userMatch[0] || []).filter(e=>/\d{4}/g.test(e)) || []).sort((a,b)=>str.indexOf(a)-str.indexOf(b))[1],
+                    VR: ((userMatch[0] || []).filter(e=>/\d{4}/g.test(e)) || []).sort((a,b)=>str.indexOf(a)-str.indexOf(b))[1],
+                    BR: ((userMatch[0] || []).filter(e=>/\d{4}/g.test(e)) || []).sort((a,b)=>str.indexOf(a)-str.indexOf(b))[0],
                     loginRegion: (userMatch[0] || [])[2],
                     gameType: (userMatch[0] || []).find(e=>e.includes('vs')) ? "versus" : "battle",
                     connectionFail: (userMatch[0] || []).find(e=>/\d\.\d{2}$/.test(e)) ? userMatch[0].find(e=>/\d\.\d{2}$/.test(e)).substr(userMatch[0].find(e=>/\d\.\d{2}$/.test(e)).search(/\d\.\d{2}/)) : false
