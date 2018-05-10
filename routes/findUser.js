c50ccfda380d1bd2a071a7c7de1c3325d4ccddb5
Timo <30553356+y21@users.noreply.github.com>
2018-05-10 22:07:07 +0200

    Codacy fixes

--- routes/findUser.js
+++ routes/findUser.js
@@ -1,4 +1,4 @@
-const { get } = require('https');
+const { get } = require("https");
 
 /**
  * findUser endpoint; get information about a player (has to be in a room!)
@@ -7,18 +7,18 @@ const { get } = require('https');
  * @param res Response object (express)
  */
 module.exports = (req, res) => {
-    if (!req.query.name) return res.json({status: 400, message: 'No name parameter provided'});
+    if (!req.query.name) return res.json({status: 400, message: "No name parameter provided"});
     get("https://wiimmfi.de/mkw/list", result => {
-        let str = '';
-        result.on('data', d => str += d);
-        result.on('end', () => {
-            str = str.substr(str.indexOf('<th>friend code</th>'));
+        let str = "";
+        result.on("data", d => str += d);
+        result.on("end", () => {
+            str = str.substr(str.indexOf("<th>friend code</th>"));
 
             // Automatically escape special regex characters
             let escapedNickname = req.query.name;
-            ['*', '^', '$', '?', '\\d', '\\w', '\\n', '\\s', '(', ')', '+', '[', ']', '-'].map(r => escapedNickname = escapedNickname.replace(new RegExp('\\'+r, 'g'), '\\' + r));
+            ["*", "^", "$", "?", "\\d", "\\w", "\\n", "\\s", "(", ")", "+", "[", "]", "-"].map(r => escapedNickname = escapedNickname.replace(new RegExp("\\"+r, "g"), "\\" + r));
 
-            const userMatch = (str.match(new RegExp('<td align="center">(CTGP|Eur|Jap|Ame)\\/\\d?<\\/td>\\s*<td align="center">.{1,16}<\\/td>\\s*<td align="center">.{1,16}<\\/td>\\s*<td align="center".{1,128}<\\/td>\\s*<td align="center">.{1,16}<\\/td>\\s*<td align="center">.{1,16}<\\/td>\\s*<td>' + ((req.query.flags || '').split(",").includes("i") ? `.*${escapedNickname}.*` : escapedNickname) + '<\\/td>', 'g')) || []).map(e=>{
+            const userMatch = (str.match(new RegExp("<td align=\"center\">(CTGP|Eur|Jap|Ame)\\/\\d?<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td align=\"center\".{1,128}<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td>" + ((req.query.flags || "").split(",").includes("i") ? `.*${escapedNickname}.*` : escapedNickname) + "<\\/td>", "g")) || []).map(e=>{
                 return e.split(/(<td align="center">|<\/td>|<td align="center">|<td align="center" title="[\w\s]+">|<td>)/)
             });
             if(!userMatch[0]) return res.json({ status: 400, message: "user not found" });
