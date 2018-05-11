162388b641ccbd430d029e75e4e6f5ad650cfdc3
Timo <30553356+y21@users.noreply.github.com>
2018-05-11 23:00:59 +0200

    Spaces around => in arrow function + semicolon

--- routes/findUser.js
+++ routes/findUser.js
@@ -18,8 +18,8 @@ module.exports = (req, res) => {
             let escapedNickname = req.query.name;
             ["*", "^", "$", "?", "\\d", "\\w", "\\n", "\\s", "(", ")", "+", "[", "]", "-"].map(r => escapedNickname = escapedNickname.replace(new RegExp("\\"+r, "g"), "\\" + r));
 
-            const userMatch = (str.match(new RegExp("<td align=\"center\">(CTGP|Eur|Jap|Ame)\\/\\d?<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td align=\"center\".{1,128}<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td>" + ((req.query.flags || "").split(",").includes("i") ? `.*${escapedNickname}.*` : escapedNickname) + "<\\/td>", "g")) || []).map(e=>{
-                return e.split(/(<td align="center">|<\/td>|<td align="center">|<td align="center" title="[\w\s]+">|<td>)/)
+            const userMatch = (str.match(new RegExp("<td align=\"center\">(CTGP|Eur|Jap|Ame)\\/\\d?<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td align=\"center\".{1,128}<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td>" + ((req.query.flags || "").split(",").includes("i") ? `.*${escapedNickname}.*` : escapedNickname) + "<\\/td>", "g")) || []).map(e => {
+                return e.split(/(<td align="center">|<\/td>|<td align="center">|<td align="center" title="[\w\s]+">|<td>)/);
             });
             if(!userMatch[0]) return res.json({ status: 400, message: "user not found" });
             res.json({
@@ -28,8 +28,8 @@ module.exports = (req, res) => {
                     VR: ((userMatch[0] || []).filter(e => /\d{4}/g.test(e)) || []).sort((a,b) => str.indexOf(a)-str.indexOf(b))[1],
                     BR: ((userMatch[0] || []).filter(e => /\d{4}/g.test(e)) || []).sort((a,b) => str.indexOf(a)-str.indexOf(b))[0],
                     loginRegion: (userMatch[0] || [])[2],
-                    gameType: (userMatch[0] || []).find(e=>e.includes("vs")) ? "versus" : "battle",
-                    connectionFail: (userMatch[0] || []).find(e=>/\d\.\d{2}$/.test(e)) ? userMatch[0].find(e => /\d\.\d{2}$/.test(e)).substr(userMatch[0].find(e => /\d\.\d{2}$/.test(e)).search(/\d\.\d{2}/)) : false,
+                    gameType: (userMatch[0] || []).find(e => e.includes("vs")) ? "versus" : "battle",
+                    connectionFail: (userMatch[0] || []).find(e => /\d\.\d{2}$/.test(e)) ? userMatch[0].find(e => /\d\.\d{2}$/.test(e)).substr(userMatch[0].find(e => /\d\.\d{2}$/.test(e)).search(/\d\.\d{2}/)) : false,
                 }
             });
         });
