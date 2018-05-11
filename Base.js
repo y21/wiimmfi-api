967af6654b883d10e391c26c04ef5417b08fd043
Timo <30553356+y21@users.noreply.github.com>
2018-05-11 22:56:10 +0200

    Use double quotes

--- Base.js
+++ Base.js
@@ -6,16 +6,16 @@ Base.utils = { };
 Base.initUtils = () => {
     return new Promise((resolve, reject) => {
         for(const f of require("fs").readdirSync("./utils/")){
-            if(f !== 'utils'){
+            if(f !== "utils"){
                 if(!existsSync(`./utils/${f}`)) reject(`File ${f} does not exist.`);
-                Base.utils[f.split('.')[0]] = require("./utils/" + f);
+                Base.utils[f.split(".")[0]] = require("./utils/" + f);
             }
         }
         resolve(Base.utils);
     });
 };
 Base.db = require("sqlite");
-Base.db.open('./database.sqlite');
+Base.db.open("./database.sqlite");
 
 
 
