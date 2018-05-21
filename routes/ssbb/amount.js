0578dfe7d524c9b6f73549c824a61516a5c91b3f
Timo <30553356+y21@users.noreply.github.com>
2018-05-21 22:55:36 +0200

    Add SSBB' amount endpoint

---
+++ routes/ssbb/amount.js
@@ -0,0 +1,18 @@
+const { db } = require("../../Base");
+
+/**
+ * Amount endpoint (ssbb);
+ *
+ * @param req Request object (express)
+ * @param res Response object (express)
+ */
+module.exports = (req, res) => {
+    db.get("select * from ssbb").then(result => {
+        const re = {
+            totalProfiles: result.totalProfiles,
+            lastCheck: result.lastEdit,
+            status: 200
+        };
+        res.json(re);
+    });
+};
