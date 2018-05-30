5246a7f690b02d3e569da7ffb75e5c9f22875b51
Timo <30553356+y21@users.noreply.github.com>
2018-05-30 23:32:27 +0200

    Add vue frontend javascript file

---
+++ docs/vue-frontend.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: "#app",
+    data: {
+        app: {
+            name: "wiimmfi-api",
+            links: {
+                github: "https://github.com/y21/wiimmfi-api/",
+                discord: "https://discord.gg/6DPWSmK/",
+                gh_releases: "https://github.com/y21/wiimmfi-api/releases",
+                about_page: "about.html"
+            }
+        }
+    }
+});
