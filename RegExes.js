b31f77eebd6713990ac500c1c8d802a21e226aa1
Timo <30553356+y21@users.noreply.github.com>
2018-06-04 19:28:10 +0200

    Seperate file for RegExes

---
+++ RegExes.js
@@ -0,0 +1,17 @@
+module.exports = {
+    mkwii: {
+        worldwides: /Worldwide *room/i,
+        continentals: /Continental *room/i,
+        privates: /Private *room/i,
+        players: /<td>.{1,28}<\/td>/,
+        ctgp: /<td align *= *"center" *> *CTGP *<\/td *>/i,
+        eur: /<td align *= *"center" *> *Eur\/2 *<\/td *>/i,
+        jap: /<td align *= *"center" *> *Jap\/0 *<\/td *>/i,
+        ame: /<td align *= *"center" *> *Ame\/1 *<\/td *>/i
+    },
+    ssbb: {
+        totalProfiles: /<td *align *= *"?center *"?> *\d+ *<\/td> *<td *align *= *"?center *"?><a *href *= *"\/game\/smashbrosxwii" *> *\d+ *<\/a>/i,
+        online: /<td *align *= *"?center *"?> *\d+ *<\/td> *<td *align *= *"?center *"?><a *href *= *"\/game\/smashbrosxwii" *> *\d+ *<\/a>/i,
+        thirty_minutes: /<td *align *= *"?center"? *> *\d+ *<\/td> *<td *align *= *"?center"? *> *<a *href *= *"\/game\/smashbrosxwii" *>\d+<\/a><\/td> *<td *align *= *center *>\d+<\/td>/i
+    }
+};
