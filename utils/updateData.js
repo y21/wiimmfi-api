20c4e0916f5dfc554a53628a1f430bf16533cf25
Timo <30553356+y21@users.noreply.github.com>
2018-06-04 20:49:55 +0200

    Change name of regex and add new information to ep

--- utils/updateData.js
+++ utils/updateData.js
@@ -69,16 +69,19 @@ try {
                     totalProfiles: ((str.match(ssbb.totalProfiles) || ["0"])[0].match(/\d+/) || ["0"])[0],
                     online: ((str.match(ssbb.totalProfiles) || ["0"])[0].match(/\d+/g) || [null, "0"])[1],
                     logins: {
-                        thirty_minutes: ((str.match(ssbb.thirty_minutes) || ["0"])[0].match(/\d+/g) || [null, null, null, "—"])[2]
+                        thirty_minutes: ((str.match(ssbb.logins) || ["0"])[0].match(/\d+/g) || [null, null, null, "—"])[2],
+                        four_hours: ((str.match(ssbb.logins) || ["0"])[0].match(/\d+/g) || [null, null, null, "—"])[3],
+                        twentyfour_hours: ((str.match(ssbb.logins) || ["0"])[0].match(/\d+/g) || [null, null, null, "—"])[4]
                     }
                 };
-                
+
                 // ---------------------
                 // Queries
                 // ---------------------
+
                 db.all("select * from ssbb").then(r => {
-                    if (r.length === 0) return db.run(`insert into ssbb values(${Number(result.totalProfiles)}, ${Number(result.online)}, '${Date.now()}', ${Number(result.logins.thirty_minutes)}, null, null, null )`).catch(console.log);
-                    else db.run(`UPDATE ssbb SET totalProfiles=${Number(result.totalProfiles)}, online=${Number(result.online)}, lastEdit='${Date.now()}', thirtyMinutes=${Number(result.logins.thirty_minutes)}`).catch(console.log);
+                    if (r.length === 0) return db.run(`insert into ssbb values(${Number(result.totalProfiles)}, ${Number(result.online)}, '${Date.now()}', ${Number(result.logins.thirty_minutes)}, ${Number(result.logins.four_hours)}, ${Number(result.logins.twentyfour_hours)}, null )`).catch(console.log);
+                    else db.run(`UPDATE ssbb SET totalProfiles=${Number(result.totalProfiles)}, online=${Number(result.online)}, lastEdit='${Date.now()}', thirtyMinutes=${Number(result.logins.thirty_minutes)}, fourHours=${Number(result.logins.four_hours)}, twentyfourHours=${Number(result.logins.twentyfour_hours)}`).catch(console.log);
                 }).catch(error => {
                     if (error.toString().includes("no such table: ssbb")) {
                         return db.run("CREATE TABLE `ssbb` ( `totalProfiles` INTEGER, `online` INTEGER, `lastEdit` TEXT, `thirtyMinutes` INTEGER, `fourHours` INTEGER, `twentyfourHours` INTEGER, `sevenDays` INTEGER )").catch(console.log);
@@ -86,7 +89,6 @@ try {
                 });
             });
         });
-
     };
 } catch (e) {
     console.log(e)
