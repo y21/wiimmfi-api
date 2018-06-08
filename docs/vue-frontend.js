de9e51701987c0a1639aad9cdca414dcf3e4bf08
Timo <30553356+y21@users.noreply.github.com>
2018-06-08 21:42:08 +0200

    Indent fixes (beautify)

--- docs/vue-frontend.js
+++ docs/vue-frontend.js
@@ -11,8 +11,7 @@ new Vue({
                 endpoints: "endpoints.html",
                 supported_games: "supported.html"
             },
-            changelog: [
-                {
+            changelog: [{
                     date: "1st June 2018",
                     contents: [
                         "name can be passed as header and as a POST parameter now"
@@ -93,27 +92,26 @@ new Vue({
                 // Mario Kart Wii
                 {
                     name: "amount",
-					http_method: "get",
+                    http_method: "get",
                     desc: "This endpoint returns the amount of available worldwides, continentals, privates, total worldwides, total continentals, total privates, total players and players in available rooms.",
                     format: "JSON",
-                    parameters: [ ],
+                    parameters: [],
                     response: "https://gist.githubusercontent.com/y21/8a742a17585ff92703b12696abc902b6/raw/215a6276f3d77a7f6ff01721d0f3a945f7bb495d/amount-endpoint.json"
                 },
                 {
                     name: "regions",
-					http_method: "get",
+                    http_method: "get",
                     desc: "This endpoint returns the amount of players logged in for each region which can be either CTGP, Jap/0, Ame/1 or Eur/2.",
                     format: "JSON",
-                    parameters: [ ],
+                    parameters: [],
                     response: "https://gist.githubusercontent.com/y21/8a742a17585ff92703b12696abc902b6/raw/215a6276f3d77a7f6ff01721d0f3a945f7bb495d/regions-endpoint.json"
                 },
                 {
                     name: "findUser",
-					http_method: "get",
+                    http_method: "get",
                     desc: "This endpoint returns information about a user such as VR, BR, etc. This may not work for every user that has weird characters in it; most likely hacked mii names. It also won't find players that are playing with 2 or more players (split-screen) if the i flag has not been set.<br/>name parameter can be either passed as a GET, POST or header.",
                     format: "JSON",
-                    parameters: [
-                        {
+                    parameters: [{
                             name: "name",
                             desc: "The mii name of the player"
                         },
@@ -126,19 +124,19 @@ new Vue({
                 },
                 {
                     name: "users",
-					http_method: "get",
+                    http_method: "get",
                     desc: "This endpoint returns an array of users in all rooms.",
                     format: "JSON",
-                    parameters: [ ],
+                    parameters: [],
                     response: "https://gist.githubusercontent.com/y21/8a742a17585ff92703b12696abc902b6/raw/215a6276f3d77a7f6ff01721d0f3a945f7bb495d/users-endpoint.json"
                 },
                 // Super Smash Bros Brawl
                 {
                     name: "ssbb/amount",
-					http_method: "get",
+                    http_method: "get",
                     desc: "This endpoint returns the amount of players that have been connected to wiimmfi once with the game super smash bros brawl",
                     format: "JSON",
-                    parameters: [ ],
+                    parameters: [],
                     response: "https://gist.githubusercontent.com/y21/8a742a17585ff92703b12696abc902b6/raw/f187af6037a498f8a1f169484170a2bd4b50a453/ssbb-amount-endpoint.json"
                 }
             ]
