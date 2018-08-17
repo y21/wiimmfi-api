8e62a26070b59dc811efbc103197065dbc03cf6d
Timo <30553356+y21@users.noreply.github.com>
2018-08-17 15:03:42 +0200

    Add RegExes#bans object for bans endpoint

--- RegExes.js
+++ RegExes.js
@@ -13,5 +13,9 @@ module.exports = {
         totalProfiles: /<td *align *= *"?center *"?> *\d+ *<\/td> *<td *align *= *"?center *"?><a *href *= *"\/game\/smashbrosxwii" *> *\d+ *<\/a>/,
         logins: /<td *align *= *"?center"? *> *\d+ *<\/td> *<td *align *= *"?center"? *> *<a *href *= *"\/game\/smashbrosxwii" *>\d+<\/a><\/td> *<td *align *= *center *>\d+<\/td><td align=center>\d+<\/td><td align=center>\d+<\/td>/i
     },
-    animal_crossing_ds: /<tr class="tr0"><td title="acrossingds">ADME<\/td>.+\s+.+/g
+    animal_crossing_ds: /<tr class="tr0"><td title="acrossingds">ADME<\/td>.+\s+.+/g,
+    bans: {
+        col1: /<tr *id *= *"?pager-\d{1,10}-\d{1,3}-0 *class *= *"?anchor *tr[01] *"?> *<td *align *= *"?center"? *>\d+.+/g,
+        col1_ban_id: /\d{4,8}(?=<\/td> *<td *align *="?center *"? *class *= *"?nobr)/g
+    }
 };
