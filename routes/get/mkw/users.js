ea2f57f07d6c51db157632c77f467b10d12f1956
Timo <30553356+y21@users.noreply.github.com>
2018-10-31 12:20:29 +0100

    fix: regex match error (mkw/users)

--- routes/get/mkw/users.js
+++ routes/get/mkw/users.js
@@ -6,7 +6,7 @@ module.exports = (req, res) => {
 
         result.on("data", d => str += d);
         result.on("end", () => {
-            const userMatch = (str.match(new RegExp("<td align=\"center\">(CTGP|Eur|Jap|Ame)\\/?\\d?<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td align=\"center\".{1,128}<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td align=\"center\">.{1,16}<\\/td>\\s*<td>.*<\\/td>", "g")) || []).map(e => {
+            const userMatch = (str.match(new RegExp("<td align=\"center\".+<td align=center>(\\d{4}|—)<\/td>\\s*<td align=center>(\\d{4}|—)<\/td>\\s*<td>.+<\/td>", "g")) || []).map(e => {
                 return e.split(/(<td align="center">|<\/td>|<td align="center">|<td align="center" title="[\w\s]+">|<td>)/);
             });
             res.send(userMatch.map(e => e[e.length-3]));
