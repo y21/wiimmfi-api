4d2d20f87ba1a9af4a7dd30f46b4e8c9215eaa76
Timo <artur.fiedler54@gmail.com>
2019-12-21 22:44:19 +0100

    cache prototype

---
+++ cache/cache.go
@@ -0,0 +1,10 @@
+package cache
+
+// Cache holds cached data
+type Cache struct {
+}
+
+// Update function fetches new data from origin API and updates internal cache
+func (cache *Cache) Update() {
+
+}
