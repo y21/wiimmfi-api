f3c495265289bc2b7c5348d47c3ca355de6240ea
Timo <artur.fiedler54@gmail.com>
2019-12-21 23:01:22 +0100

    add more cache structures

--- cache/cache.go
+++ cache/cache.go
@@ -1,7 +1,39 @@
 package cache
 
+type mkwGameData struct {
+	Worldwides   int `json:"worldwides"`
+	Continentals int `json:"continentals"`
+	Privates     int `json:"privates"`
+	Players      int `json:"players"`
+}
+
+type mkw struct {
+	FullName string      `json:"fullName"`
+	Data     mkwGameData `json:"data"`
+}
+
+type loginData struct {
+	ThirtyMinutes   int `json:"thirty_minutes"`
+	FourHours       int `json:"four_hours"`
+	TwentyfourHours int `json:"twentyfour_hours"`
+}
+
+type gameData struct {
+	TotalProfiles int       `json:"totalProfiles"`
+	Online        int       `json:"online"`
+	Logins        loginData `json:"logins"`
+}
+
+type game struct {
+	FullName string   `json:"fullName"`
+	Data     gameData `json:"data"`
+}
+
 // Cache holds cached data
 type Cache struct {
+	LastCheck string `json:"lastCheck"`
+	Mkw       mkw    `json:"mkw"`
+	Games     []game `json:"games"`
 }
 
 // Update function fetches new data from origin API and updates internal cache
