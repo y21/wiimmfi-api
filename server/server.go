f6407d657e5ceaf850af3076ef67aef66b9afa0f
Timo <btimo8822@gmail.com>
2020-03-23 19:46:52 +0100

    fix bugs

--- server/server.go
+++ server/server.go
@@ -22,24 +22,38 @@ func main() {
 	httpCache.Update()
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "unofficial wiimmfi api v%s\n\nfind source code/api documentation on github: %s\nsupport on discord: y21#0909", version, "https://github.com/y21/wiimmfi-api")
-	})
-
-	router.HandleFunc("/api/"+apiVersion+"/{game}/overview", func(w http.ResponseWriter, r *http.Request) {
-		params := mux.Vars(r)
-		w.Header().Set("Content-Type", "application/json")
-		if params["game"] == "mkw" {
-			json.NewEncoder(w).Encode(&httpCache.Mkw.Data.Rooms)
-		} else {
-			json.NewEncoder(w).Encode(httpCache.Games[params["game"]].Data)
+		_, err := fmt.Fprintf(w, "unofficial wiimmfi api v%s\n\nfind source code/api documentation on github: %s\nsupport on discord: y21#0909", version, "https://github.com/y21/wiimmfi-api")
+		if err != nil {
+			fmt.Println(err)
 		}
 	})
 
-	router.HandleFunc("/api/"+apiVersion+"/mkw/regions", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/api/" + apiVersion + "/{game}/overview", func(w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(&httpCache.Mkw.Data.Regions)
+		var err error
+		if params["game"] == "mkw" {
+			err = json.NewEncoder(w).Encode(&httpCache.Mkw.Data.Rooms)
+		} else {
+			err = json.NewEncoder(w).Encode(httpCache.Games[params["game"]].Data)
+		}
+
+		if err != nil {
+			fmt.Println(err)
+		}
+	})
+
+	router.HandleFunc("/api/" + apiVersion + "/mkw/regions", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		err := json.NewEncoder(w).Encode(&httpCache.Mkw.Data.Regions)
+		if err != nil {
+			fmt.Println(err)
+		}
 	})
 
 	fmt.Println("Webserver started")
-	http.ListenAndServe(":3000", router)
+	err := http.ListenAndServe(":3000", router)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
